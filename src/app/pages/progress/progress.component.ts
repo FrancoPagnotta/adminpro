65e6bfcfcf1390d36dd46021df0e7d3dfde63bea
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-07 12:25:06 -0300

    Create logic for change the progress

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
+  percentageProgress: number = 5;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  getPercentageProgress(): string {
+    return `${this.percentageProgress}%`;
   }
 
+  changePercentage(value: number): void {
+    this.percentageProgress = this.percentageProgress + value;
+  }
 }
+
