683a97ce23a9846d5aebaa8ee58e50f4fac01de5
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-08 15:54:38 -0300

    Implement graphic chart tempalte and logic

--- src/app/pages/graphic1/graphic1.component.ts
+++ src/app/pages/graphic1/graphic1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-graphic1',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graphic1.component.css']
 })
 export class Graphic1Component implements OnInit {
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ] }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
   constructor() { }
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { ComponentsModule } from '../components/components.module';
+import { NgChartsModule } from 'ng2-charts';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Graphic1Component } from './graphic1/graphic1.component';
 import { NotpageComponent } from './notpage/notpage.component';
 import { ProgressComponent } from './progress/progress.component';
 import { PagesComponent } from './pages.component';
-import { RouterModule } from '@angular/router';
-import { ComponentsModule } from '../components/components.module';
 
 
 
@@ -23,7 +25,8 @@ import { ComponentsModule } from '../components/components.module';
     CommonModule,
     RouterModule,
     SharedModule,
-    ComponentsModule
+    ComponentsModule,
+    NgChartsModule
   ],
   exports: [
     DashboardComponent,
