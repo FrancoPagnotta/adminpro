e4f92433a5e0db65ab014ee288ee8ba31a0dea7d
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-12 15:59:54 -0300

    Create sidebar service

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Menu } from '../shared/interfaces/menu';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+  menu: Menu[] = [
+    {
+      title: 'Dashboard',
+      icon: 'mdi mdi-gauge', 
+      subMenu: [
+        {
+          title: 'Main',
+          url: '/'
+        },
+        {
+          title: 'Progressbar',
+          url: '/progress'
+        },
+        {
+          title: 'Graphics',
+          url: '/graphic1'
+        },
+      ]
+    }
+  ];
+
+  constructor() { }
+}
