b08943788bc114a1bdb3192a476e754608842584
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-12 17:14:42 -0300

    Create reactive form for login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  logIn(): void {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      this.router.navigateByUrl('/dashboard'); 
+    };
   }
 
 }
