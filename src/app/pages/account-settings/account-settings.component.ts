5efde2b29f563d47745a3654b637cf0e5be4129e
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-12 12:18:26 -0300

    Create method checkCurrentTheme in account settings component

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -6,17 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-settings.component.css']
 })
 export class AccountSettingsComponent implements OnInit {
-  public element = document.getElementById('theme');
+  public linkTheme = document.querySelector('#theme');
+  public selectors!: NodeListOf<Element>; 
 
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.selectors = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+
   }
   
   changeTheme(theme: string): void {
     const url = `./assets/css/colors/${theme}.css`;
-    this.element?.setAttribute('href', url);
+    this.linkTheme?.setAttribute('href', url);
     localStorage.setItem('theme', url);
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme(): void {
+    this.selectors.forEach(selector => {
+      selector.classList.remove('working');
+      const optionTheme = selector.getAttribute('data-theme');
+      const optionThemeUrl = `./assets/css/colors/${optionTheme}.css`;
+      const linkThemeUrl = this.linkTheme?.getAttribute('href');
+
+      if (optionThemeUrl === linkThemeUrl) {
+        selector.classList.add('working');
+      }
+    });
   }
 
 }
