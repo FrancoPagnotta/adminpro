87c8d8b6c5c96720c8f370192740a46d49ea7148
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-13 11:15:19 -0300

    Simple demo of handling observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -10,6 +11,28 @@ export class RxjsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const obs$ = new Observable((observer: Subscriber<unknown>) => {
+      let i = 0;
+      const interval = setInterval(() => {
+        i++;
+        observer.next(i)
+        
+        if (i === 4) {
+          clearInterval(interval);
+          observer.complete();
+        }
+
+        if (i === 2) {
+          observer.error('I is equal to 2');
+        }
+      }, 1000)
+    });
+
+    obs$.subscribe(
+      resp => console.log(resp), // Esta primer funcion se ejecuta cuando el next emite un valor
+      error => console.log(error), // Esta funcion se ejecuta cuando el error emite un valor. Cuando se emite el error, el complete no se va a ejecutar, corta en el error.
+      () => console.log('obs ended') // Y esta funcion se ejecuta cuando el complete se ejecuta, que en este caso si lo implementamos en el obs de arriba.
+    )
   }
 
 }
