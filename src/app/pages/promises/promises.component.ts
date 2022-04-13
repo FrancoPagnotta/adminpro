7f5afd43638288ba8ec95958d318d59962e4514b
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-13 00:13:56 -0300

    Simple demonstration of the performance of promises

--- src/app/pages/promises/promises.component.ts
+++ src/app/pages/promises/promises.component.ts
@@ -10,6 +10,22 @@ export class PromisesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const promise = new Promise((resolve, reject) => { // Este procedimiento es sincrono
+      if ( 3 > 25) {
+        resolve('Hello world');
+      } else {
+        reject('something wrong');
+      }
+    });
+
+    promise.then((message) => { // En cambio este procedimiento y el del catch, es asincrono
+      console.log(message);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+
+    console.log('Init end');
   }
 
 }
